562b0c5c41bc236b160fc2cbae3c0deadcffb654
Joao <jo4o.fds@gmail.com>
2023-05-11 12:47:37 -0300

    ajustes em index.php

--- router.php
+++ router.php
@@ -5,11 +5,10 @@ $path = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-//
+// mata aplicação caso exista um arquivo com nome da rota
 if ($path !== '/' && file_exists(__DIR__ . '/public' . $path)) {
     return false;
 }
 
-unset($path);
-
+// incluindo index
 require_once __DIR__ . '/public/index.php';
